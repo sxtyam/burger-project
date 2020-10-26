de13ca70a4e8f1543e976c2fface1a7bf4ff6f0f
Satyam Gaur <sgsatyamg@gmail.com>
2020-10-26 11:25:42 +0530

    Add basic side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import classes from './Layout.module.css'
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Auxiliary>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
