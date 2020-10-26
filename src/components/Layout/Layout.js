75fb1c4d52eb05fca1398599f83d69a66775f598
Satyam Gaur <sgsatyamg@gmail.com>
2020-10-26 10:44:06 +0530

    Add basic navbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
 
 import classes from './Layout.module.css'
+import Auxiliary from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
     <Auxiliary>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
